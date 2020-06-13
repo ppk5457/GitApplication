f1e897efb40aa6efe43278e2e8a2ca96dd240f4a
ppk5457 <developer013@163.com>
2020-06-13 08:16:11 +0800

    add SplashActivity.java

--- app/src/main/java/cn/edu/sc/gitapplication/MainActivity.java
+++ app/src/main/java/cn/edu/sc/gitapplication/MainActivity.java
@@ -2,13 +2,26 @@ package cn.edu.sc.gitapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView txtLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txtLogin=findViewById(R.id.txtLogin);
+        txtLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent();
+                intent.putExtra("message","Hi Android!");
+                startActivity(intent);
+            }
+        });
     }
 }
